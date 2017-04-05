24dccb9b2359705f4482c25f7ea8f36fccc09718
Ray Guo <ihahoo@163.com>
2017-04-05 16:39:50 +0800

    For react-router4

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { AppContainer } from 'react-hot-loader';
+import createHistory from 'history/createBrowserHistory';
 import configureStore from './store';
-import routes from './routes';
+import App from './containers/App';
 
 /* eslint no-underscore-dangle: 0 */
 const initialState = window.__INITIAL_STATE__;
-const store = configureStore(initialState);
-const history = syncHistoryWithStore(browserHistory, store, {
-  selectLocationState(state) {
-    return state.get('routing').toJS();
-  }
-});
-
-const rootEl = document.getElementById('container');
+const history = createHistory();
+const store = configureStore(initialState, history);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router routes={routes()} history={history} />
-  </Provider>,
-  rootEl
+  <AppContainer>
+    <App store={store} history={history} />
+  </AppContainer>,
+  document.getElementById('container')
 );
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const NextApp = require('./containers/App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('container')
+    );
+  });
+}
--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import { fromJS } from 'immutable';
 import rootReducer from './reducers';
 
-export default function configureStore(initialState) {
+export default function configureStore(initialState, history) {
   const middleware = compose(
-    applyMiddleware(thunk),
-
+    applyMiddleware(
+      thunk,
+      routerMiddleware(history)
+    ),
     process.env.NODE_ENV === 'development' &&
     typeof window === 'object' &&
     typeof window.devToolsExtension !== 'undefined'
@@ -20,7 +23,6 @@ export default function configureStore(initialState) {
     if (module.hot) {
       module.hot.accept('./reducers', () => {
         const nextReducer = require('./reducers').default;
-
         store.replaceReducer(nextReducer);
       });
     }
