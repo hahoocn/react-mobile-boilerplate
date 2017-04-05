5eaa398b4788ca8a7bd7279b189b6e595abe9f46
Ray Guo <ihahoo@163.com>
2017-04-05 16:42:52 +0800

    Separate api

---
+++ src/containers/Home/api.js
@@ -0,0 +1,19 @@
+import request from '../../utils/api';
+
+const showHelloApi = (name) => {
+  const result = new Promise((resolve) => {
+    setTimeout(() => {
+      const resJson = { infoAsync: 'Async loading' };
+      resJson.name = name;
+      resolve(resJson);
+    }, 1000);
+  });
+  return result;
+};
+
+const showMoviesApi = () => request({
+  url: 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json',
+  crossOrigin: true
+});
+
+export { showHelloApi, showMoviesApi };
